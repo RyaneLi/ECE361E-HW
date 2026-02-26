8025f75b6ac44017123589455accfa7a8669c933
Ryane Li <ryane.li@warriorlife.net>
2026-02-26 14:20:29 -0600

    P1 model: see vgg11.pt

--- HW3_files/models/vgg16.py
+++ HW3_files/models/vgg16.py
@@ -1,8 +1,9 @@
 import torch.nn as nn
 
 cfg = {
-    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
 }
+
 
 class VGG16(nn.Module):
     def __init__(self, vgg_name="VGG16"):
@@ -12,3 +13,24 @@ class VGG16(nn.Module):
             nn.Linear(512, 512),
             nn.ReLU(),
             nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+        )
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.ReLU()]
+                in_channels = x
+        return nn.Sequential(*layers)
